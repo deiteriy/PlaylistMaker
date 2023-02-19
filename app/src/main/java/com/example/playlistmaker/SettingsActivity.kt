af78fd8b16ec3dea19d63ce27b4dbede2c46033c
deiteriy <komarov.dmit@gmail.com>
2023-02-19 19:34:18 +0400

    Новое: -работают поля в меню настроек
    Нужно исправить:
    -кнопка "Написать в поддержку" должна открывать все по умолчанию, а не через chooser
    -цвет кнопок меню в ночном режиме

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,12 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +15,38 @@ class SettingsActivity : AppCompatActivity() {
         returnArrow.setOnClickListener {
             finish()
         }
+
+        val shareApp = findViewById<TextView>(R.id.share_app)
+        shareApp.setOnClickListener {
+            val message = getString(R.string.share_app_link)
+            val shareTitle = getString(R.string.share_title)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.type = "text/plain"
+            startActivity(Intent.createChooser(shareIntent, shareTitle))
+        }
+
+        val mailToSupport = findViewById<TextView>(R.id.support)
+        mailToSupport.setOnClickListener {
+            val message = getString(R.string.support_message)
+            val subject = getString(R.string.support_subject)
+            val mailToSupportIntent = Intent(Intent.ACTION_SENDTO)
+            val userEmail = getString(R.string.user_email)
+            mailToSupportIntent.data = Uri.parse("mailto:")
+         //   mailToSupportIntent.addCategory(Intent.CATEGORY_APP_EMAIL)
+            mailToSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(userEmail))
+            mailToSupportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            mailToSupportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            startActivity(mailToSupportIntent)
+        }
+
+        val showUserAgreement = findViewById<TextView>(R.id.user_agreement)
+        showUserAgreement.setOnClickListener {
+            val link = getString(R.string.user_agreement_link)
+            val browseIntent = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+            startActivity(browseIntent)
+        }
+
 
     }
 }
