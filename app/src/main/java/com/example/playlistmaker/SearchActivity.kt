169b09b256f1d0ae5fae4a3b4fb9bccc336f6ddf
deiteriy <komarov.dmit@gmail.com>
2023-02-24 20:09:42 +0400

    Новое: - цвета скорректированы

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,13 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -17,20 +19,27 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-
-    val linearLayout = findViewById<LinearLayout>(R.id.container)
-    val inputSearchText = findViewById<EditText>(R.id.inputSearch)
-    val clearButton = findViewById<ImageView>(R.id.clearIcon)
-       
-
-    clearButton.setOnClickListener {
-        inputSearchText.setText("")
-    }
-
-    val searchTextWatcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            // empty
+        val returnArrow = findViewById<ImageView>(R.id.arrow_back)
+        returnArrow.setOnClickListener {
+            finish()
         }
+
+
+        val linearLayout = findViewById<LinearLayout>(R.id.container)
+        val inputSearchText = findViewById<EditText>(R.id.inputSearch)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+
+        clearButton.setOnClickListener {
+            inputSearchText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+
+        val searchTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
 
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
             clearButton.visibility = clearButtonVisibility(s)
