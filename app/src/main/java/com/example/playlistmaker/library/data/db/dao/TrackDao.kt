6d7e9d754cabf82055b3b3a7c531ef8036b6ed8c
deiteriy <komarov.dmit@gmail.com>
2023-08-13 21:12:06 +0400

    Добавил запросы в TrackDao

--- app/src/main/java/com/example/playlistmaker/library/data/db/dao/TrackDao.kt
+++ app/src/main/java/com/example/playlistmaker/library/data/db/dao/TrackDao.kt
@@ -1,5 +1,6 @@
 package com.example.playlistmaker.library.data.db.dao
 
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
@@ -10,6 +11,12 @@ interface TrackDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertTrack(track: TrackEntity)
 
+    @Delete
+    suspend fun deleteTrack(track: TrackEntity)
+
     @Query("SELECT * FROM track_table")
     suspend fun getTracks(): List<TrackEntity>
+
+    @Query("SELECT trackId FROM track_table")
+    suspend fun getTracksId()
 }
