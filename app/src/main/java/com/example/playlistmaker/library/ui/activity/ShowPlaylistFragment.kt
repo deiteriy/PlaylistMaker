78ed55ac080df1f38a32116e6fd470f310b5e4d5
deiteriy <komarov.dmit@gmail.com>
2023-09-13 00:16:57 +0400

    Добавлен фрагмент — экран плейлиста

---
+++ app/src/main/java/com/example/playlistmaker/library/ui/activity/ShowPlaylistFragment.kt
@@ -0,0 +1,30 @@
+package com.example.playlistmaker.library.ui.activity
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.playlistmaker.databinding.FragmentShowPlaylistBinding
+
+
+class ShowPlaylistFragment : Fragment() {
+
+    private lateinit var binding: FragmentShowPlaylistBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentShowPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+
+
+
+}
