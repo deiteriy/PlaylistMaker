9e2fbc140837f633ed4bb37ec5b717c07a3096ae
deiteriy <komarov.dmit@gmail.com>
2024-02-25 15:54:52 +0400

    Добавил файлы для PlaybackButtonView и ее атрибутов

---
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlaybackButtonView.kt
@@ -0,0 +1,15 @@
+package com.example.playlistmaker.player.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+
+class PlaybackButtonView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    @AttrRes defStyleAttr: Int = 0,
+    @StyleRes defStyleRes: Int = 0
+) : View(context, attrs, defStyleAttr, defStyleRes) {
+}
