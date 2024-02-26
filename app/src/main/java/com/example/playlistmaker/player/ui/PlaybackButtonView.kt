13ebb0ddb5bd3da46009b2587fb0064a9d0b189d
deiteriy <komarov.dmit@gmail.com>
2024-02-26 23:21:10 +0400

    Переопределены методы onDraw и onSizeChanged для PlaybackButtonView

--- app/src/main/java/com/example/playlistmaker/player/ui/PlaybackButtonView.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/PlaybackButtonView.kt
@@ -1,10 +1,15 @@
 package com.example.playlistmaker.player.ui
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
+import androidx.core.graphics.drawable.toBitmap
+import com.example.playlistmaker.R
 
 class PlaybackButtonView @JvmOverloads constructor(
     context: Context,
@@ -12,4 +17,33 @@ class PlaybackButtonView @JvmOverloads constructor(
     @AttrRes defStyleAttr: Int = 0,
     @StyleRes defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
+
+    private val imageBitmap: Bitmap?
+    private var imageRect = RectF(0f, 0f, 0f, 0f)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        imageRect = RectF(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+
+    }
+    override fun onDraw(canvas: Canvas?) {
+        if (imageBitmap != null) {
+            canvas?.drawBitmap(imageBitmap, null, imageRect, null)
+        }
+    }
+
+    init {
+        context.theme.obtainStyledAttributes(
+            attrs,
+            R.styleable.PlaybackButtonView,
+            defStyleAttr,
+            defStyleRes
+        ).apply {
+            try {
+                imageBitmap = getDrawable(R.styleable.PlaybackButtonView_playbackImageResId)?.toBitmap()
+            } finally {
+                recycle()
+            }
+        }
+    }
 }
